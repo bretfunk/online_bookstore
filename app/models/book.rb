1dbd37a741c9b06e65b04a794872ae5b0f26d55d
BFunk <bret.e.funk@gmail.com>
2017-11-06 12:13:42 -0700

    testing method ideas

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,8 +5,31 @@ class Book < ApplicationRecord
   has_many :book_formats
   has_many :book_format_types, through: :book_formats
 
+  #Search options:
+  #:title_only (defaults to false).  If true, only return results from rule #3 above.
+  #:book_format_type_id (defaults to nil).  If supplied, only return books that are available in a format that matches the supplied type id.
+  #:book_format_physical (defaults to nil).   If supplied as true or false, only return books that are available in a format whose “physical” field matches the supplied argument.  This filter is not applied if the argument is not present or nil.
+
+  #The title_only and book_format options are not exclusive of each other, so
+  #Book.search('Karamazov', title_only: true, book_format_physical: true) should return all physical books whose title matches that term.
 
   def self.search(query, options)
+    new_options = {
+      title_only: options[:title_only] || false,
+      book_format_type_id: options[:book_format_type_id] || nil,
+      book_format_physical: options[:book_format_physical] || nil
+    }
+
+    if new_options[:title_only] && new_options[:book_format_physical]
+      puts "title and physical"
+    elsif new_options[:title_only]
+      puts "title only"
+    elsif new_options[:book_format_type_id]
+      puts "book format type"
+    elsif new_options[:book_format_physical]
+      puts "physical?"
+    end
+
   end
 
   def book_format_types
