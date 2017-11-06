ccc3df34db792eb41ee35d51cc2b7c046ece3e70
BFunk <bret.e.funk@gmail.com>
2017-11-05 17:51:26 -0700

    passing publisher model tests

--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Publisher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it "is valid with valid attributes" do
+      publisher = create(:publisher)
+      create_list(:book, 3, publisher_id: publisher.id)
+
+      expect(publisher).to be_valid
+      expect(publisher.name).to eq("Publisher")
+      expect(publisher.books.count).to eq(3)
+    end
+  end
+
+  context "associations" do
+    it { should have_many(:books) }
+  end
 end
