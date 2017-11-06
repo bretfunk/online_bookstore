c8d29d23c9b54367712a67d53f29722b1b3462f3
BFunk <bret.e.funk@gmail.com>
2017-11-05 18:07:26 -0700

    passing book_format_types model tests

--- spec/models/book_format_type_spec.rb
+++ spec/models/book_format_type_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe BookFormatType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it "is valid with valid attributes" do
+      book = create(:book)
+      book_format_type = create(:book_format_type)
+      create(:book_format, book_id: book.id, book_format_type_id: book_format_type.id)
+
+      expect(book_format_type).to be_valid
+      expect(book_format_type.name).to eq("Kindle")
+      expect(book_format_type.physical).to eq(false)
+      expect(book_format_type.books.first.title).to eq("BookTitle")
+
+    end
+  end
+
+  context "associations" do
+    it { should have_many(:book_formats) }
+    it { should have_many(:books) }
+  end
 end
